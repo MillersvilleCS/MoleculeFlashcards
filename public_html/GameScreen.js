275d48146dff37ac541288afcd1dcf60ffa45b85
William Gervasio <wpgervasio@gmail.com>
2013-07-29 14:34:37 -0400

    updated mouse listener

--- public_html/GameScreen.js
+++ public_html/GameScreen.js
@@ -105,8 +105,8 @@ GameScreen.prototype =
         var pointLight = new THREE.PointLight( 0xFFFFFF );
 
     	// set its position
-    	pointLight.position.x = 10;
-    	pointLight.position.y = 50;
+    	pointLight.position.x = 0;
+    	pointLight.position.y = 0;
     	pointLight.position.z = 130;
 
     	// add to the scene
