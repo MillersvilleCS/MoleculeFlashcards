8c470f0b8e7f3b8e5b4ddab9be60e6c9ef8beff2
Connor Mahaffey <connorsmahaffey@gmail.com>
2013-08-01 17:51:24 -0400

    Moved Button JS to MoleculeGame (for now) - modified some CSS

--- public_html/MoleculeGame.js
+++ public_html/MoleculeGame.js
@@ -14,3 +14,33 @@ MoleculeGame.prototype.update = function ( delta )
 {
     this.screen.onUpdate ( delta );
 };
+
+/*
+    Handles game-to-game logic for buttons. Can be moved to another
+    class if you need to.
+*/
+MoleculeGame.prototype.buttonLogic = function( button )
+{
+    switch ( button )
+    {
+        case 'START':
+            $('#mainMenuUI').fadeOut(500);
+            break;
+
+        case 'HIGH SCORES':
+            $('canvas').css('display', 'none');
+            $('#mainMenuUI').fadeOut(500);
+            $('#gameUI').fadeOut(500);
+            $('#highScoreUI').css('display', 'block');
+            break;
+            
+        case 'Main Menu':
+            $('#mainMenuUI').fadeIn(500);
+            $('#gameUI').fadeIn(500);
+            $('#highScoreUI').delay(500).fadeOut(1);
+            $('canvas').delay(500).fadeIn(1);
+            break;
+        default:
+            alert('Not Yet Implemented!');
+    }
+};
