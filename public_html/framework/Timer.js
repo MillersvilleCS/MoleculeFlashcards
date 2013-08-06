4237afd73ad1da0455443b132a500cf3ade363e4
exscitech <exscitech@haptics3.cslabs.millersville.edu>
2013-08-06 17:24:55 -0400

    Defined fields in the constructor (where they should be) instead of the prototype. Also removed creation of Data objects.

--- public_html/framework/Timer.js
+++ public_html/framework/Timer.js
@@ -1,39 +1,43 @@
 
 Timer = function ( )
 {
-
+    this.startTime = 0,
+    this.stopTime =  0,
+    this.running =   false;
 };
 
 Timer.prototype = {
     constructor: Timer,
-    startTime: 0,
-    stopTime: 0,
-    running: false,
+
     start: function ( )
     {
         this.running = true;
-        this.startTime = new Date ( );
+        this.startTime = Date.now ( );
     },
+
     stop: function ( )
     {
         this.running = false;
-        this.stopTime = new Date ( );
+        this.stopTime = Date.now ( );
     },
+
     reset: function ( )
     {
-        var time = new Date ( );
+        var time = Date.now ( );
         this.startTime = time;
         this.stopTime = time;
     },
+
     getElapsedMs: function ( )
     {
         if ( this.running )
         {
-            return new Date - this.startTime;
+            return Date.now () - this.startTime;
         }
 
         return this.stopTime - this.startTime;
     },
+
     getElapsedSec: function ( )
     {
         return Math.floor ( this.getElapsedMs ( ) / 1000 );
