2d098fe657ff7da623f69f446cf6a7e8baa7d123
Connor Mahaffey <connorsmahaffey@gmail.com>
2013-08-06 10:51:41 -0400

    Started working on having molecules be pulled from the server and displayed

--- public_html/framework/TextLoader.js
+++ public_html/framework/TextLoader.js
@@ -6,32 +6,14 @@ TextLoader = function ( )
 TextLoader.FILE_READY = 4;
 TextLoader.FILE_FOUND = 200;
 
-TextLoader.loadText = function ( url )
+/*
+ * Url to retrieve and another function to call on success. 
+ * If there is an error, callback function gets 'ERROR'
+ */
+TextLoader.loadText = function ( url , callback )
 {
-    /*
-    var textFile;
-    
-    if ( window.XMLHttpRequest )
-    {
-        textFile = new XMLHttpRequest ( );
-    }
-    textFile.onreadystatechange = function ( )
-    {
-        if ( textFile.readyState === TextLoader.FILE_READY && 
-                textFile.status === TextLoader.FILE_FOUND )
-        {
-           allText = textFile.responseText;
-           lines = textFile.responseText.split ( '\n' );
-           console.log('These are split correctly! Shows them weird in console, good to parse!');
-           console.log(lines);
-        }    
-    };
-    textFile.open( "GET", url, true );
-    textFile.setRequestHeader( "Content-Type", "text/plain" );
-    textFile.send( null );
-    */
     var textFile = $.ajax( url )
-        .done(function() { console.log(textFile.responseText); })
-        .fail(function() { alert("error"); })
-        .always(function() { alert("complete"); });
+        .done(function() { callback( textFile.responseText ); })
+        .fail(function() { callback( 'ERROR' ); })
+        .always(function() {});
 };
