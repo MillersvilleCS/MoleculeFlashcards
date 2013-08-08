701b11497cad2ab02ef54ce72e37000bf60a663e
Connor Mahaffey <connorsmahaffey@gmail.com>
2013-08-08 15:41:01 -0400

    Added tutorial

--- public_html/src/js/MenuScreen.js
+++ public_html/src/js/MenuScreen.js
@@ -27,12 +27,37 @@ MenuScreen.prototype.onResume = function ( )
     $ ( '#mainMenuUI' ).fadeIn ( 500 );
 };
 
+MenuScreen.prototype.tutorial = function ( )
+{
+    $ ( '#mainMenuUI' ).fadeOut ( 200 );
+    $ ( '#tutorialUI' ).delay ( 200 ).fadeIn ( 300 );
+    $ ( '#rightPanel' ).delay ( 2000 ).fadeIn ( 300 );
+    //$ ( '#rightPanel' ).delay ( 10000 ).fadeOut ( 300 );
+    //$ ( '#tutorialUI' ).delay ( 12000 ).fadeOut ( 300 );
+    //$ ( '#mainMenuUI' ).delay ( 12000 ).fadeIn ( 300 );
+};
+
+MenuScreen.prototype.endTutorial = function ( )
+{
+    $ ( '#rightPanel' ).fadeOut ( 300 );
+    $ ( '#tutorialUI' ).fadeOut ( 300 );
+    $ ( '#mainMenuUI' ).delay ( 300 ).fadeIn ( 300 );
+};
+
 MenuScreen.prototype.buttonLogic = function ( button )
 {
     switch ( button )
     {
         case 'Play':
             return 'game';
+
+        case 'TUTORIAL':
+            this.tutorial ( );
+            break;
+
+        case 'Tutorial Return':
+            this.endTutorial ( );
+            break;
 
         case 'HIGH SCORES':
             return 'score';
