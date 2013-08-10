5df076d7fe7fad7f052eb4071ead1e9eea4e16fd
William Gervasio <wpgervasio@gmail.com>
2013-08-10 00:10:32 -0400

    Update Timer.js

--- public_html/framework/Timer.js
+++ public_html/framework/Timer.js
@@ -1,13 +1,11 @@
-
 Timer = function ( ) {
-
+    this.startTime = 0;
+    this.stopTime = 0;
+    this.running = false;
 };
 
 Timer.prototype = {
     constructor: Timer,
-    startTime: 0,
-    stopTime: 0,
-    running: false,
     start: function ( ) {
         'use strict';
         this.running = true;
