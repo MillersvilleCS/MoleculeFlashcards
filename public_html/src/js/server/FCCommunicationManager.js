05e0dd0137070f3f18b00d68ba29e3a403e1f87b
Connor Mahaffey <connorsmahaffey@gmail.com>
2013-08-14 12:31:29 -0400

    Updated FCCommManager to latest version with Sams code

--- public_html/src/js/server/FCCommunicationManager.js
+++ public_html/src/js/server/FCCommunicationManager.js
@@ -13,7 +13,6 @@ FCCommunicationManager.MEDIA_IMAGE = 1;
 
 FCCommunicationManager.login = function ( email, password , callback) {
     'use strict';
-    /* Tested */
     email = email.toLowerCase ();
     var passwordHash = hex_md5 (password + email);
 
@@ -27,20 +26,18 @@ FCCommunicationManager.login = function ( email, password , callback) {
 
 FCCommunicationManager.availableGames = function ( auth, callback ) {
     'use strict';
-    /* FCCommunicationManager */
     var requestObject = {};
     requestObject.request_type = 'get_avail_flashcard_games';
-    requestObject.auth = auth;
+    requestObject.authenticator = auth;
 
     FCCommunicationManager.post ( FCCommunicationManager.REQUEST_HANDLER_URL, requestObject, callback );
 };
 
 FCCommunicationManager.loadFlashcardGame = function ( auth, gameID, callback ) {
     'use strict';
-    /* Tested - Ours isn't on there yet I guess */
     var requestObject = {};
     requestObject.request_type = 'load_flashcard_game';
-    requestObject.auth = auth;
+    requestObject.authenticator = auth;
     requestObject.game_id = gameID;
 
     FCCommunicationManager.post ( FCCommunicationManager.REQUEST_HANDLER_URL, requestObject, callback );
@@ -50,8 +47,9 @@ FCCommunicationManager.endFlashcardGame = function ( auth, gameSessionID, callba
     'use strict';
     var requestObject = {};
     requestObject.request_type = 'end_flashcard_game';
-    requestObject.auth = auth;
+    requestObject.authenticator = auth;
     requestObject.game_session_id = gameSessionID;
+    requestObject.game_time = gameTime;
 
     FCCommunicationManager.post ( FCCommunicationManager.REQUEST_HANDLER_URL, requestObject, callback );
 };
@@ -60,7 +58,7 @@ FCCommunicationManager.submitFlashcardAnswer = function ( auth, gameSessionID, q
     'use strict';
     var requestObject = {};
     requestObject.request_type = 'submit_flashcard_answer';
-    requestObject.auth = auth;
+    requestObject.authenticator = auth;
     requestObject.game_session_id = gameSessionID;
     requestObject.question_id = questionID;
     requestObject.answer = answer;
@@ -71,7 +69,6 @@ FCCommunicationManager.submitFlashcardAnswer = function ( auth, gameSessionID, q
 
 FCCommunicationManager.getMedia = function ( gameSessionID, mediaType, questionID, callback ) {
     'use strict';
-    /* Tested - Pulled back a PDB + other info Sam talked about */
     var requestObject = {};
     requestObject.gsi = gameSessionID;
     requestObject.mt = mediaType;
