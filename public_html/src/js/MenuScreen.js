dbb481e6dea21ae5a6ce20194e8942cac242892f
Connor Mahaffey <connorsmahaffey@gmail.com>
2013-08-19 14:20:43 -0400

    First draft of MenuScreen now functional

--- public_html/src/js/MenuScreen.js
+++ public_html/src/js/MenuScreen.js
@@ -9,13 +9,14 @@
     
     var $element;
     var topics;
-    var currentTopic = 0;
+    var $lastTopicElement;
 
-    var TOPIC_HTML = '<div class = \'topic\' id = \'$uniqueID\' >' +
+    var TOPIC_HTML = '<div class = \'topic\' data-id = \'$uniqueID\' >' +
                         '<img class = \'topicImage\' src = \'$imageSrc\' width = \'114\' height = \'94\' >' +
                         '<b>$title</b>' + '<br />' +
                         '<div class = \'topicDescription\'>$description</div>' +
                      '</div>';
+    var SCORES_HTML = '$rank $name $score <br />';
 
     MenuScreen.prototype = Object.create (Screen.prototype);
     MenuScreen.prototype.constructor = MenuScreen;
@@ -47,7 +48,6 @@
 
     MenuScreen.prototype.tempImageChange = function ( imageSrc ) {
         /* Until this is running on the exscitech server, we need to give an absolute path */
-
         return 'http://exscitech.gcl.cis.udel.edu/' + imageSrc.substr(2, imageSrc.length - 2);
     };
 
@@ -73,10 +73,32 @@
                 topics[i].name, 
                 topics[i].description,
                 this.tempImageChange ( topics[i].image ),
-                'divID' + i
+                i
             ];
             this.insertInfo( keys, values, TOPIC_HTML, '#topicList' );
-            topics[i].divID = i;
+            topics[i].dataID = i;
+        }
+        $lastTopicElement = $($('#topicList').children()[0]);
+        $lastTopicElement.css('background-color', 'grey');
+        this.changeRightPanel ( topics[0] );
+    };
+
+    MenuScreen.prototype.changeRightPanel = function ( topic ) {
+        $('#timeLimit').html('Time Limit: ' + Timer.getDigitalRep( topic.time_limit / 1000 ) );
+        $('#highScores').html('');
+        var currScores = topic.high_scores;
+        for(var i = 0; i < currScores.length; ++i) {
+            var keys = [
+                '$rank',
+                '$name',
+                '$score'
+            ];
+            var values = [
+                currScores[i].rank,
+                currScores[i].username,
+                currScores[i].score
+            ];
+            this.insertInfo( keys, values, SCORES_HTML, '#highScores' );
         }
     };
 
@@ -84,9 +106,6 @@
         $ ('#mainMenuUI').fadeOut (200);
         $ ('#tutorialUI').delay (200).fadeIn (300);
         $ ('#rightPanel').delay (2000).fadeIn (300);
-        //$ ( '#rightPanel' ).delay ( 10000 ).fadeOut ( 300 );
-        //$ ( '#tutorialUI' ).delay ( 12000 ).fadeOut ( 300 );
-        //$ ( '#mainMenuUI' ).delay ( 12000 ).fadeIn ( 300 );
     };
 
     MenuScreen.prototype.endTutorial = function ( ) {
@@ -95,63 +114,44 @@
         $ ('#mainMenuUI').delay (300).fadeIn (300);
     };
 
-//    MenuScreen.prototype.buttonLogic = function (button) {
-//        switch (button) {
-//            case 'Play':
-//                return 'game';
-//
-//            case 'TUTORIAL':
-//                this.tutorial ( );
-//                break;
-//
-//            case 'Tutorial Return':
-//                this.endTutorial ( );
-//                break;
-//
-//            case 'HIGH SCORES':
-//                return 'score';
-//
-//            case 'Main Menu':
-//                return 'menu';
-//
-//            default:
-//                //alert( 'Not Yet Implemented!' );
-//        }
-//    };
-
     function enableButtons (menuScreen) {
-        $('#mainMenuUI .button[data-logic="play"]').on('click', function () {
-            var screenChangeEvent = jQuery.Event("screenChange");
-            screenChangeEvent.screenID = "game";
+        $('#mainMenuUI .button[data-logic=\'play\']').on('click', function () {
+            var screenChangeEvent = jQuery.Event('screenChange');
+            screenChangeEvent.screenID = 'game';
             menuScreen.getElement().trigger(screenChangeEvent);
         });
         
-        $('#mainMenuUI .button[data-logic="tutorial"]').on('click', function () {
+        $('#mainMenuUI .button[data-logic=\'tutorial\']').on('click', function () {
             menuScreen.tutorial ( );
         });
         
-        $('#mainMenuUI .button[data-logic="endTutorial"]').on('click', function () {
+        $('#mainMenuUI .button[data-logic=\'endTutorial\']').on('click', function () {
             menuScreen.endTutorial ( );
         });
         
-        $('#mainMenuUI .button[data-logic="scores"]').on('click', function () {
-            var screenChangeEvent = jQuery.Event("screenChange");
-            screenChangeEvent.screenID = "score";
+        $('#mainMenuUI .button[data-logic=\'scores\']').on('click', function () {
+            var screenChangeEvent = jQuery.Event('screenChange');
+            screenChangeEvent.screenID = 'score';
             menuScreen.getElement().trigger(screenChangeEvent);
         });
         
-        $('#mainMenuUI .button[data-logic="menu"]').on('click', function () {
-            var screenChangeEvent = jQuery.Event("screenChange");
-            screenChangeEvent.screenID = "menu";
+        $('#mainMenuUI .button[data-logic=\'menu\']').on('click', function () {
+            var screenChangeEvent = jQuery.Event('screenChange');
+            screenChangeEvent.screenID = 'menu';
             menuScreen.getElement().trigger(screenChangeEvent);
         });
         
+        $('#topicList').on('click', '.topic[data-id]' , function (e) {
+            menuScreen.changeRightPanel ( topics[$(this).data('id')] );
+            $lastTopicElement.css('background-color', 'transparent');
+            $(this).css('background-color', 'grey');
+            $lastTopicElement = $(this);
+        });
     }
 
     function disableButtons ( ) {
         $('#mainMenuUI .button').off('click');
     }
     
-    // export MenuScreen
     window.MenuScreen = MenuScreen;
 }) ( );
